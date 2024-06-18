ccf3b526982f51136f4a3623e8a0a1f156366e4c
Evan Tung <dragonejt@gmail.com>
2024-06-18 08:42:15 -0700

    Separate Insights into UserInsights and CommunityInsights

--- src/insights.py
+++ src/insights.py
@@ -2,24 +2,38 @@ from dataclasses import dataclass
 import json
 
 
-@dataclass
 class Insights:
+    def serialize(self):
+        return json.dumps(self.__dict__)
+
+
+@dataclass
+class UserInsights(Insights):
     user: str
     psycho_hazard: bool
 
-    def serialize(self):
-        return json.dumps(self.__dict__)
+
+@dataclass
+class CommunityInsights(Insights):
+    community: str
+
 
 class InsightsDB:
     def __init__(self, db) -> None:
         self.db = db
 
-    def get(self, user: str) -> Insights:
+    def get_user(self, user: str) -> UserInsights:
         insights = json.loads(self.db.get(user))
-        return Insights(
-            user=insights.get("user"),
-            psycho_hazard=insights.get("psycho_hazard")
+        return UserInsights(
+            user=insights.get("user"), psycho_hazard=insights.get("psycho_hazard")
         )
 
-    def put(self, insights: Insights) -> None:
+    def put_user(self, insights: UserInsights) -> None:
         self.db.put(insights.user, insights.serialize())
+
+    def get_community(self, community: str) -> CommunityInsights:
+        insights = json.loads(self.db.get(community))
+        return CommunityInsights(community=insights.get("community"))
+
+    def put_community(self, insights: CommunityInsights) -> None:
+        self.db.put(insights.community, insights.serialize())
