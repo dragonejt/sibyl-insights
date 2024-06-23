a15a393f5e13cdb0935ae1086f447e96e659fc85
Evan Tung <dragonejt@gmail.com>
2024-06-23 00:03:37 -0700

    feat: Add Auth

---
+++ src/clients/auth.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+from js import fetch
+from pyodide.ffi import to_js
+
+
+async def auth(base_url: str, authorization: str, community_id: str) -> bool:
+    response = await fetch(
+        f"{base_url}/community?id={community_id}",
+        method="HEAD",
+        headers=to_js({"Authorization": authorization, "User-Agent": "sibyl-insights"}),
+    )
+    return response.status == HTTPStatus.OK.value
--- src/entry.py
+++ src/entry.py
@@ -1,7 +1,17 @@
+from http import HTTPStatus
 from js import Response
+from urllib.parse import urlparse, parse_qs
+from clients.auth import auth
 from insights import InsightsDB
 
 
-async def on_fetch(request, env):
+async def on_fetch(request, env) -> Response:
+    community_id = parse_qs(urlparse(request.url).query).get("id")[0]
+    if not await auth(
+        env.BACKEND_URL, request.headers.get("Authorization"), community_id
+    ):
+        return Response.new(
+            HTTPStatus.UNAUTHORIZED.description, status=HTTPStatus.UNAUTHORIZED.value
+        )
     insights_db = InsightsDB(env.insightsdb)
     return Response.new("Hello World!")
